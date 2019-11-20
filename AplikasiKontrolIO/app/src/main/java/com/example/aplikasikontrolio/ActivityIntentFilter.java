08bb534720b595b713c2d172207bdbb3d9eb083a
AbramFernandoH <abramfernando18@gmail.com>
2019-11-20 12:29:27 +0700

    Tutorial 3

---
+++ AplikasiKontrolIO/app/src/main/java/com/example/aplikasikontrolio/ActivityIntentFilter.java
@@ -0,0 +1,35 @@
+package com.example.aplikasikontrolio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityIntentFilter extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intentfilter);
+        Button btnSend = (Button) findViewById(R.id.sendMail);
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// Membuat objek Intent
+                Intent si = new Intent(Intent.ACTION_SEND);
+// Mengatur MIME Type
+                si.setType("message/rfc822");
+// Mengatur data nilai untuk Alamat E-Mail
+                si.putExtra(Intent.EXTRA_EMAIL, new String[]{"tirtasouja@gmail.com"});
+// Mengatur data nilai untuk Subject E-Mail
+                si.putExtra(Intent.EXTRA_SUBJECT, "Welcome to PNJ CBD Class");
+// Mengatur data nilai untuk Badan E-Mail
+                si.putExtra(Intent.EXTRA_TEXT, "Hi Students, Welcome to My Class");
+/* Menjalankan Activity dengan parameter fungsi createChooser dengan
+parameter objek Intent */
+                startActivity(Intent.createChooser(si, "Choose Mail App"));
+            }
+        });
+    }
+}
---
+++ AplikasiKontrolIO/app/src/main/java/com/example/aplikasikontrolio/ActivityOutput.java
@@ -0,0 +1,24 @@
+package com.example.aplikasikontrolio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityOutput extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_output);
+        TextView result = (TextView)findViewById(R.id.vHasil);
+// Mengambil nilai Intent dari activity sebelumnya, yakni Activity1
+        Intent intent = getIntent();
+/* Mengambil nilai atribut yang diangkut Intent dari Activity1 lalu
+disimpan ke sebuah variabel bertipe string dengan nama addition */
+        String addition = (String)intent.getSerializableExtra("jumlah");
+// Mengatur nilai TextField result dengan nilai didalam variabel addition
+        result.setText(addition);
+    }
+}
