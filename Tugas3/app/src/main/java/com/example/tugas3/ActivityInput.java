46b65a1e448d74ee693b6191e429f59bc163df5d
AbramFernandoH <abramfernando18@gmail.com>
2019-11-21 10:10:24 +0700

    tutorial

---
+++ Tugas3/app/src/main/java/com/example/tugas3/ActivityInput.java
@@ -0,0 +1,37 @@
+package com.example.tugas3;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ActivityInput extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input);
+        final EditText firstNum = (EditText)findViewById(R.id.angkaSatu);
+        final EditText secNum = (EditText)findViewById(R.id.angkaDua);
+        Button btnAdd = (Button)findViewById(R.id.btnTambah);
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// Memberi nilai ke dua buah variabel yakni num1 dan num2
+// Nilai diambil dari nilai yang dimasukkan pengguna di TextField
+                int num1 = Integer.parseInt(firstNum.getText().toString());
+                int num2 = Integer.parseInt(secNum.getText().toString());
+// Membuat objek Intent
+                Intent intent = new
+                        Intent(ActivityInput.this,ActivityOutput.class);
+/* Mendeklarasikan atribut Intent yakni jumlah dengan nilai hasil
+penjumlahan num1 dan num2 */
+                intent.putExtra("jumlah",num1+" + "+num2+" = "+(num1+num2));
+// Menjalankan activity dengan parameter Intent
+                        startActivity(intent);
+            }
+        });
+    }
+}
---
+++ Tugas3/app/src/main/java/com/example/tugas3/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.tugas3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView header;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        header = (TextView) findViewById(R.id.txtHeader);
+    }
+    public void launchSecondActivity(View view) {
+// Membuat objek Intent
+        Intent intent = new Intent(this, SecondActivity.class);
+// Menjalankan Activity dengan parameter Intent
+        startActivity(intent);
+    }
+}
